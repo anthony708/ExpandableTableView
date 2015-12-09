9824a74929fa9199d7f23fc79c099ab38055f12d
Anthony708 <anthony78@live.cn>
2015-12-09 14:16:49 +0800

    Fix bug of the CustomCellDelegate

--- Expandable/ViewController.swift
+++ Expandable/ViewController.swift
@@ -8,18 +8,270 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CustomCellDelegate {
 
+    // MARK: IBOutlet Properties
+    
+    @IBOutlet weak var tblExpandable: UITableView!
+    
+    var cellDescriptors: NSMutableArray!
+    var visibleRowsPerSection = [[Int]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        configureTableView()
+        loadCellDescription()
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
+    // MARK: Custom Functions
+    
+    func configureTableView() {
+        tblExpandable.delegate = self
+        tblExpandable.dataSource = self
+        tblExpandable.tableFooterView = UIView(frame: CGRectZero)
+        
+        tblExpandable.registerNib(UINib(nibName: "NormalCell", bundle: nil), forCellReuseIdentifier: "idCellNormal")
+        tblExpandable.registerNib(UINib(nibName: "TextfieldCell", bundle: nil), forCellReuseIdentifier: "idCellTextfield")
+        tblExpandable.registerNib(UINib(nibName: "DatePickerCell", bundle: nil), forCellReuseIdentifier: "idCellDatePicker")
+        tblExpandable.registerNib(UINib(nibName: "SwitchCell", bundle: nil), forCellReuseIdentifier: "idCellSwitch")
+        tblExpandable.registerNib(UINib(nibName: "ValuePickerCell", bundle: nil), forCellReuseIdentifier: "idCellValuePicker")
+        tblExpandable.registerNib(UINib(nibName: "SliderCell", bundle: nil), forCellReuseIdentifier: "idCellSlider")
+    }
+    
+    func loadCellDescription() {
+        if let path = NSBundle.mainBundle().pathForResource("CellDescriptor", ofType: "plist") {
+            cellDescriptors = NSMutableArray(contentsOfFile: path)
+            getIndicesOfVisibleRows()
+            tblExpandable.reloadData()
+        }
+    }
+    
+    func getIndicesOfVisibleRows() {
+        visibleRowsPerSection.removeAll()
+        
+        //遍历单元格描述数组
+        for currentSectionCells in cellDescriptors {
+            // 暂存每个section中, isVisible = true 的行号
+            var visibleRows = [Int]()
+            
+            for row in 0...((currentSectionCells as! [[String: AnyObject]]).count - 1) {
+                //检查每个单元格的isVisib属性是否为true
+                if currentSectionCells[row]["isVisible"] as! Bool == true {
+                    visibleRows.append(row)
+                }
+            }
+            // 将所有标记为可见的单元格行号保存到该数组中
+            // 首次家在描述文件后, 该数组值为[[0, 3, 5], [0, 5], [0]]
+            visibleRowsPerSection.append(visibleRows)
+        }
+    }
+    
+    // 函数返回值为一个字典
+    func getCellDescriptorForIndexPath(indexPath: NSIndexPath) -> [String: AnyObject] {
+        // 从visibleRowPerSection中获取可见行的索引值
+        let indexOfVisibleRow = visibleRowsPerSection[indexPath.section][indexPath.row]
+        
+        // 在cellDescriptors数组中查找并返回适当的单元格描述信息
+        let cellDescriptor = cellDescriptors[indexPath.section][indexOfVisibleRow] as! [String: AnyObject]
+        return cellDescriptor
+    }
 
+    
+    // MARK: UITableView Delegate and Datasource Functions
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        if cellDescriptors != nil {
+            return cellDescriptors.count
+        } else {
+            return 0
+        }
+    }
+    
+    // 指定每个section的行数
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return visibleRowsPerSection[section].count
+    }
+    
+    
+    // 指定每个section的标题
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Personal"
+        case 1:
+            return "Preferences"
+        default:
+            return "Work Experience"
+        }
+    }
+    
+    // 显示实际单元格
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let currentCellDescriptor = getCellDescriptorForIndexPath(indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier(currentCellDescriptor["cellIdentifier"] as! String, forIndexPath: indexPath) as! CustomCell
+        
+        if currentCellDescriptor["cellIdentifier"] as! String == "idCellNormal" {
+            if let primaryTitle = currentCellDescriptor["primaryTitle"] {
+                cell.textLabel?.text = primaryTitle as? String
+            }
+            if let secondaryTitle = currentCellDescriptor["secondaryTitle"] {
+                cell.textLabel?.text = secondaryTitle as? String
+            }
+        } else if currentCellDescriptor["cellIdentifier"] as! String == "idCellTextfield" {
+            cell.textField.placeholder = currentCellDescriptor["primaryTitle"] as? String
+        }
+        
+        else if currentCellDescriptor["cellIdentifier"] as! String == "idCellSwitch" {
+            cell.lblSwitchLabel.text = currentCellDescriptor["primaryTitle"] as? String
+            
+            let value = currentCellDescriptor["value"] as? String
+            cell.swMaritalStatus.on = (value == "true") ? true : false
+        }
+        else if currentCellDescriptor["cellIdentifier"] as! String == "idCellValuePicker" {
+            cell.textLabel?.text = currentCellDescriptor["primaryTitle"] as? String
+        }
+        else if currentCellDescriptor["cellIdentifier"] as! String == "idCellSlider" {
+            let value = currentCellDescriptor["value"] as! String
+            cell.slExperienceLevel.value = (value as NSString).floatValue
+        }
+        
+        cell.delegate = self
+        return cell
+    }
+
+    
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        let currentCellDescriptor = getCellDescriptorForIndexPath(indexPath)
+        
+        switch currentCellDescriptor["cellIdentifier"] as! String {
+        case "idCellNormal":
+            return 60.0
+            
+        case "idCellDatePicker":
+            return 270.0
+            
+        default:
+            return 44.0
+        }
+
+    }
+    
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        // 获取索引值
+        let indexOfTappedRow = visibleRowsPerSection[indexPath.section][indexPath.row]
+        
+        // 检查是否允许展开, 以flag标志位指示
+        if cellDescriptors[indexPath.section][indexOfTappedRow]["isExpandable"] as! Bool == true {
+            var shouldExpandAndShowSubRows = false
+            if cellDescriptors[indexPath.section][indexOfTappedRow]["isExpanded"] as! Bool == false {
+                shouldExpandAndShowSubRows = true
+            }
+            // 更新cellDescriptors数组中isExpanded的属性
+            cellDescriptors[indexPath.section][indexOfTappedRow].setValue(shouldExpandAndShowSubRows, forKey: "isExpanded")
+            
+            // 当单元格展开时, 显示其他附加的不可见行; 否则, 隐藏那些附加行
+            for i in (indexOfTappedRow + 1)...(indexOfTappedRow + (cellDescriptors[indexPath.section][indexOfTappedRow]["additionalRows"] as! Int)) {
+                cellDescriptors[indexPath.section][i].setValue(shouldExpandAndShowSubRows, forKey: "isVisible")
+            }
+        } else {
+            if cellDescriptors[indexPath.section][indexOfTappedRow]["cellIdentifier"] as! String == "idCellValuePicker" {
+                var indexOfParentCell: Int!
+                
+                for var i=indexOfTappedRow - 1; i>=0; --i {
+                    if cellDescriptors[indexPath.section][i]["isExpandable"] as! Bool == true {
+                        indexOfParentCell = i
+                        break
+                    }
+                }
+                
+                cellDescriptors[indexPath.section][indexOfParentCell].setValue((tblExpandable.cellForRowAtIndexPath(indexPath) as! CustomCell).textLabel?.text, forKey: "primaryTitle")
+                cellDescriptors[indexPath.section][indexOfParentCell].setValue(false, forKey: "isExpanded")
+                
+                for i in (indexOfParentCell + 1)...(indexOfParentCell + (cellDescriptors[indexPath.section][indexOfParentCell]["additionalRows"] as! Int)) {
+                    cellDescriptors[indexPath.section][i].setValue(false, forKey: "isVisible")
+                }
+            }
+        }
+
+        
+        // 改变isVisible属性值之后, 会改变可见行总数, 因此重新询问可见行的索引值
+        getIndicesOfVisibleRows()
+        tblExpandable.reloadSections(NSIndexSet(index: indexPath.section), withRowAnimation: .Fade)
+    }
+    
+    // MARK: CustomCellDelegate Functions
+    
+    func dateWasSelected(selectedDateString: String) {
+        let dateCellSection = 0
+        let dateCellRow = 3
+        
+        cellDescriptors[dateCellSection][dateCellRow].setValue(selectedDateString, forKey: "primaryTitle")
+        tblExpandable.reloadData()
+    }
+    
+    
+    func maritalStatusSwitchChangedState(isOn: Bool) {
+        let maritalSwitchCellSection = 0
+        let maritalSwitchCellRow = 6
+        
+        let valueToStore = (isOn) ? "true" : "false"
+        let valueToDisplay = (isOn) ? "Married" : "Single"
+        
+        cellDescriptors[maritalSwitchCellSection][maritalSwitchCellRow].setValue(valueToStore, forKey: "value")
+        cellDescriptors[maritalSwitchCellSection][maritalSwitchCellRow - 1].setValue(valueToDisplay, forKey: "primaryTitle")
+        tblExpandable.reloadData()
+    }
+    
+    
+    func textfieldTextWasChanged(newText: String, parentCell: CustomCell) {
+        let parentCellIndexPath = tblExpandable.indexPathForCell(parentCell)
+        
+        let currentFullname = cellDescriptors[0][0]["primaryTitle"] as! String
+        let fullnameParts = currentFullname.componentsSeparatedByString(" ")
+        
+        var newFullname = ""
+        
+        if parentCellIndexPath?.row == 1 {
+            if fullnameParts.count == 2 {
+                newFullname = "\(newText) \(fullnameParts[1])"
+            }
+            else {
+                newFullname = newText
+            }
+        }
+        else {
+            newFullname = "\(fullnameParts[0]) \(newText)"
+        }
+        
+        cellDescriptors[0][0].setValue(newFullname, forKey: "primaryTitle")
+        tblExpandable.reloadData()
+    }
+    
+    
+    func sliderDidChangeValue(newSliderValue: String) {
+        cellDescriptors[2][0].setValue(newSliderValue, forKey: "primaryTitle")
+        cellDescriptors[2][1].setValue(newSliderValue, forKey: "value")
+        
+        tblExpandable.reloadSections(NSIndexSet(index: 2), withRowAnimation: UITableViewRowAnimation.None)
+    }
+
+    
 }
 
